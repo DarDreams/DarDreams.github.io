2e9573dbd27fab85691eaf14ac32e9a8e7ae174a
DarDreams <dardreams+regs@gmail.com>
2022-10-09 13:16:04 +0200

    09.10.2022 13:16:04

--- tv/js/index2.js
+++ tv/js/index2.js
@@ -1,35 +1,45 @@
 $(".tv_tnt").hide();
 $(".tv_bitva").hide();
-$(".tv_ataka").hide();
+$(".tv_punch").hide();
 $(".menu").hide();
 $(".overlay").hide();
 
-$(".tnt").click(function() {
+$(this).click(function() {
     $(".tv_bitva").hide();
-    $(".tv_ataka").hide();
-    $(".tv_tnt").show();
+    $(".tv_punch").hide();
+    $(this).show();
 });
 
 $(".bitva").click(function() {
     $(".tv_bitva").show();
     $(".tv_tnt").hide();
-    $(".tv_ataka").hide();
+    $(".tv_punch").hide();
     });
 
  $(".ataka").click(function() {
     $(".tv_bitva").hide();
     $(".tv_tnt").unload();
-    $(".tv_ataka").show();
+    $(".tv_punch").show();
     });
 
 $(".hamburger").click(function() {
    // $(".button").show();
     $(".menu").toggle();
     $(".overlay").toggle();
+    
+    
+
+       if ($(".container").css("filter") == "blur(0px)") {
+        $(".container").css("filter","blur(10px)")} else {
+        $(".container").css("filter","blur(0px)")    
+        }
+    
+
     });
 
 $(".overlay").click(function(){
     $(".overlay").toggle();
     $(".menu").toggle();
+    $(".container").css("filter","blur(0px)");
 });
     
