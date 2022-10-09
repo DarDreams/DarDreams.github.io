fc28453d43e21fbafc4b71d277780d73b9735ecf
DarDreams <dardreams+regs@gmail.com>
2022-10-09 16:22:53 +0200

    09.10.2022 16:22:53

--- tv/js/index2.js
+++ tv/js/index2.js
@@ -4,28 +4,38 @@ $(".tv_punch").hide();
 $(".menu").hide();
 $(".overlay").hide();
 
-$(this).click(function() {
-    $(".tv_bitva").hide();
-    $(".tv_punch").hide();
-    $(this).show();
-});
 
-$(".bitva").click(function() {
-    $(".tv_bitva").show();
-    $(".tv_tnt").hide();
-    $(".tv_punch").hide();
+  
+//   $(".button_mini").each(function(i){
+//     $(this).on("click",function(){
+//         $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text())
+//     });
+//   });
+
+
+
+$(".tnt").click(function() {
+     $(".tv_bitva").hide();
+     $(".tv_tnt").show();
+     $(".tv_punch").hide();
     });
 
- $(".ataka").click(function() {
-    $(".tv_bitva").hide();
-    $(".tv_tnt").unload();
-    $(".tv_punch").show();
-    });
+    $(".bitva").click(function() {
+        $(".tv_bitva").show();
+        $(".tv_tnt").hide();
+        $(".tv_punch").hide();
+        });
 
-$(".hamburger").click(function() {
-   // $(".button").show();
-    $(".menu").toggle();
-    $(".overlay").toggle();
+
+    $(".punch").click(function() {
+        $(".tv_bitva").hide();
+        $(".tv_tnt").unload();
+        $(".tv_punch").show();
+        });
+
+    $(".hamburger").click(function() {
+        $(".menu").toggle();
+        $(".overlay").toggle();
     
     
 
