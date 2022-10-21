c25c703a5327340bf204ef13cd54b991c76ee0bb
DarDreams <dardreams+regs@gmail.com>
2022-10-21 19:52:01 +0200

    21.10.2022 19:52:01

--- tv/js/index2.js
+++ tv/js/index2.js
@@ -3,6 +3,7 @@ $(".tv_bitva").hide();
 $(".tv_punch").hide();
 $(".overlay").hide();
 
+//document.querySelectorAll('.tnt>a:after').setAttribute("width","60px");
     
     $("li").click(function(){
         $(".tv_bitva").fadeOut();
@@ -12,20 +13,25 @@ $(".overlay").hide();
        $("li.tnt, li.bitva, li.punch").removeClass("animate__bounceOutLeft");
        $("li.tnt, li.bitva, li.punch").removeClass("animate__bounceInLeft");
        $("li.tnt, li.bitva, li.punch").removeClass("animate__animated");
-        $(".tv_"+$(this).attr("class")).fadeIn();   
+        $(".tv_"+$(this).attr("class")).fadeIn(); 
+        $("li.tnt, li.bitva, li.punch").addClass("animate__animated");
+        console.clear();  
+        console.log($(".tnt>a").css("width"));
     });
         
  function HideMenuToggle() {
     //console.log($("li").attr("class"));
-    if ( $("li").attr("class") == "tnt    animate__animated animate__bounceInLeft") {    
+    if ( $("li").attr("class") == "tnt animate__animated animate__bounceInLeft") {    
     $("li.tnt, li.bitva, li.punch").removeClass("animate__bounceInLeft");
     $("li.tnt, li.bitva, li.punch").addClass("animate__bounceOutLeft");
      $(".overlay").fadeOut(1000);
+     $(".container").css("filter","blur(0px)");
      
         } else {       
             $("li.tnt, li.bitva, li.punch").removeClass("animate__bounceOutLeft");
             $("li.tnt, li.bitva, li.punch").addClass("animate__bounceInLeft");
             $(".overlay").fadeIn();
+            $(".container").css("filter","blur(10px)");
             
             
         }
@@ -34,12 +40,9 @@ $(".overlay").hide();
  
         
     $(".hamburger").click(function() {
-       // $(".hamburger").fadeOut();
+
         HideMenuToggle();    
-       if ($(".container").css("filter") == "blur(0px)") {
-           $(".container").css("filter","blur(10px)")} else {
-           $(".container").css("filter","blur(0px)")    
-        }
+
     });
 
 $(".overlay").click(function(){
@@ -47,6 +50,5 @@ $(".overlay").click(function(){
     $("li.tnt, li.bitva, li.punch").addClass("animate__bounceOutLeft");
     $(".overlay").fadeOut(1000);
     $(".container").css("filter","blur(0px)");
+    //console.log("dd")
 });
-    
-
