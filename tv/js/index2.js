edee99f3404b60b51bbefee16d80b040d6106df7
DarDreams <dardreams+regs@gmail.com>
2022-10-30 13:25:08 +0100

    30.10.2022 13:25:07

--- tv/js/index2.js
+++ tv/js/index2.js
@@ -1,8 +1,8 @@
-//$(".tv_tnt").hide();
+console.clear();
+$(".tv_tnt").hide();
 $(".tv_bitva").hide();
 $(".tv_punch").hide();
 $(".overlay").hide();
-
 
 
     if (($("body").width() > $(".tv").width()) || ($("body").height()>$(".tv").height())) 
@@ -16,16 +16,13 @@ $(".overlay").hide();
 }
 
 
+//console.clear();
+// console.log("TOP = " + topTV + "px");
+// console.log("BODY_HEGIHT = " + $("body").height() + "-" + $(".tv").height());
 
-
-
-
-console.clear();
-console.log("TOP = "+topTV+"px");
-console.log("BODY_HEGIHT = "+$("body").height()+"-"+$(".tv").height());
 
 //document.querySelectorAll('.tnt>a:after').setAttribute("width","60px");
-    
+
     $("li").click(function(){
         $(".tv_bitva").fadeOut();
         $(".tv_tnt")  .fadeOut();
@@ -36,34 +33,29 @@ console.log("BODY_HEGIHT = "+$("body").height()+"-"+$(".tv").height());
        $("li.tnt, li.bitva, li.punch").removeClass("animate__animated");
         $(".tv_"+$(this).attr("class")).fadeIn(); 
         $("li.tnt, li.bitva, li.punch").addClass("animate__animated");
-        console.clear();  
         console.log($(".tnt>a").css("width"));
     });
-        
+    
  function HideMenuToggle() {
     //console.log($("li").attr("class"));
     if ( $("li").attr("class") == "tnt animate__animated animate__bounceInLeft") {    
-    $("li.tnt, li.bitva, li.punch").removeClass("animate__bounceInLeft");
-    $("li.tnt, li.bitva, li.punch").addClass("animate__bounceOutLeft");
-     $(".overlay").fadeOut(1000);
-     $(".container").css("filter","blur(0px)");
-     
-        } else {       
-            $("li.tnt, li.bitva, li.punch").removeClass("animate__bounceOutLeft");
-            $("li.tnt, li.bitva, li.punch").addClass("animate__bounceInLeft");
-            $(".overlay").fadeIn();
-            $(".container").css("filter","blur(10px)");
-            
-            
+        $("li.tnt, li.bitva, li.punch").removeClass("animate__bounceInLeft");
+        $("li.tnt, li.bitva, li.punch").addClass("animate__bounceOutLeft");
+        $(".overlay").fadeOut(1000);
+        //$(".hamburger").css("z-index","5");
+        $(".tv").css("filter","blur(0px)");
+     } else {       
+        $("li.tnt, li.bitva, li.punch").removeClass("animate__bounceOutLeft");
+        $("li.tnt, li.bitva, li.punch").addClass("animate__bounceInLeft");
+        $(".overlay").fadeIn();
+       // $(".hamburger").css("z-index","-1");
+        $(".tv").css("filter","blur(10px)");           
         }
-       // $(".hamburger").show();
+         //$(".hamburger").show();
  }
- 
-        
+    
     $(".hamburger").click(function() {
-
         HideMenuToggle();    
-
     });
 
 $(".overlay").click(function(){
@@ -71,5 +63,6 @@ $(".overlay").click(function(){
     $("li.tnt, li.bitva, li.punch").addClass("animate__bounceOutLeft");
     $(".overlay").fadeOut(1000);
     $(".container").css("filter","blur(0px)");
-    //console.log("dd")
+    console.log("dd!")
 });
+console.log("GAVNO PABOTAET!");
