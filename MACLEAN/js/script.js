6c3fc921b92b0fc309e383ce301215a5b75aa9d6
DarDreams <dardreams+regs@gmail.com>
2022-12-29 14:09:34 +0100

    29.12.2022 14:09:33

--- MACLEAN/js/script.js
+++ MACLEAN/js/script.js
@@ -17,3 +17,31 @@
 // counters.forEach((item, i) => {
 //     lines[i].style.width = item.innerHTML;
 // });
+
+$(document).ready(function(){
+
+$("#first").click(function(){
+    
+    const ID_SCROLL = "#inicio";
+    $("html, body").animate({scrollTop: $(ID_SCROLL).offset().top+"px"});
+    return false;
+  });
+
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 300) {
+      $(".pageup").fadeIn();
+    } else {
+      $(".pageup").fadeOut();
+    }
+  });
+
+  $(".pageup").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: 0+"px"});
+    return false;
+  });
+  
+
+
+});
