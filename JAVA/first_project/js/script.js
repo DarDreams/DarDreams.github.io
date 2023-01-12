9fc594b6a80a1f067545d7feba6e5910f8e9ac7b
DarDreams <dardreams@gmail.com>
2023-01-12 10:23:06 +0100

    12.01.2023 10:23:05

--- JAVA/first_project/js/script.js
+++ JAVA/first_project/js/script.js
@@ -8,13 +8,13 @@ const leftBorderWidth = 1;
 number = 10;
 console.log(number);
 
-const obj = {
-    a:50
-};
+// const obj = {
+//     a:50
+// };
 
-obj.a = 10;
+// obj.a = 10;
 
-console.log(obj);
+// console.log(obj);
 
 {
     let result = 50;
@@ -22,3 +22,14 @@ console.log(obj);
 
 //console.log(result);
 
+const obj = {
+  name: "John",
+  Age: 25,
+  isMarried: false  
+};
+
+// console.log(obj.name);
+console.log(obj ["name"]);
+
+let arr = ['plum.png', "orange.jpg", 6, "apple.bmp", [], {}];
+console.log(arr[1]); 
