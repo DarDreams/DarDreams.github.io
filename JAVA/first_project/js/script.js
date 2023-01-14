cef16509f38d07c98c3d1c675088669a07d65843
DarDreams <dardreams@gmail.com>
2023-01-14 16:12:13 +0100

    14.01.2023 16:12:12

--- JAVA/first_project/js/script.js
+++ JAVA/first_project/js/script.js
@@ -98,16 +98,18 @@ getTimeFromMinutes(180);
 
 function findMaxNumber(a,b,c,d) {
   let numb = ["a","b","c","d"];
-  for (let i = 1; i < 4;i++) {
+  for (let i = 0; i < 3; i++) {
     if (typeof(numb[i]) != "number" || numb[i] == "NaN") {
-        console.log(typeof(numb[i]));
-        //return("0");
+        console.log(0);
+        return("0");
+    } else {
+         console.log(Math.max(a,b,c,d)+"sd");
+         return(Math.max(a,b,c,d));
     }
 }
 
-    console.log(Math.max(a,b,c,d));
-    return(Math.max(a,b,c,d));
+    
     
 }
 
-findMaxNumber(1,5,3);
+findMaxNumber(1,5,3,4);
