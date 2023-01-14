39a46ef92c89389eaebf06f97ca56cefb7fafbac
DarDreams <dardreams@gmail.com>
2023-01-14 20:38:09 +0100

    14.01.2023 20:38:09

--- JAVA/first_project/js/test.js
+++ JAVA/first_project/js/test.js
@@ -1,11 +1,13 @@
 function findMaxNumber(a,b,c,d) {
 
   let numb = [a,b,c,d];
-  console.log('Number.isInteger(numb[i]);', Number.isInteger(numb[3]));
+  console.log('Number.isInteger(numb[i]) - ', Number.isInteger(numb[1]));
   
   for (let i = 0; i < 3; i++) {
-    if (typeof(numb[i]) != "number" || typeof(numb[i]) == NaN || Number.isInteger(numb[i]) == false) {
-    return(0);
+    if (typeof(numb[i]) != "number" || Number.isInteger(numb[i]) == true || typeof(numb[i]) == "NaN") {
+        console.log('0', 0);
+        
+    
     } else {
          console.log('Math.max(a,b,c,d)', Math.max(a,b,c,d));
          return(Math.max(a,b,c,d));
@@ -13,4 +15,4 @@ function findMaxNumber(a,b,c,d) {
 }
 }
 
-findMaxNumber(1,5,3,"5");
+findMaxNumber(1,"5",3,6);
