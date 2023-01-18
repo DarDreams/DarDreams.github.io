00498a6197ee7e166540a62f6a91ea6219be9564
DarDreams <dardreams@gmail.com>
2023-01-18 15:25:29 +0100

    18.01.2023 15:25:29

--- JAVA/first_project/js/caliber.js
+++ JAVA/first_project/js/caliber.js
@@ -1,79 +1,145 @@
 var caliber = {
-    replaceNumbers:function(numbers) {
-    var res = ""; 
-    var sNumb = {
-            0: "{Chr(186)}",
-            1: "{Chr(178)}",
-            2: "{Chr(179)}",
-            3: "{Chr(8307)}",
-            4: "{Chr(8308)}",
-            5: "{Chr(8309)}",
-            6: "{Chr(8310)}",
-            7: "{Chr(8311)}",
-            8: "{Chr(8312)}",
-            9: "{Chr(8313)}",
-            colon: "˙",
-            dot: "`",
-            plus: "⁺",
-            minus: "⁻"
-        };
-        
-    for (var value in sNumb) {
-        var n = 0;
-        for (var key = 0; key < numbers.length; key++) {
-            //console.log(`KEY = ${key} \nsNUMB.key = ${sNumb[key]}\nVALUE = ${value}\nNUMBER_LEN=${numbers.length}`);
-            // return
-            if (key == value) {
-                if (n >= numbers.length) {break;}
-                n++;
-                numbers = numbers.replace(key,sNumb[key]);
-                console.log(key,"  PIZDAAAAAAAAAAAAAAA   ", numbers);
-                
-                //console.log(numbers.replace(1,"{Chr(8304)}"));
-                //console.log(res);
-                //return res;
-            } else {
-               // console.log("NERAVNO");
-                    
-                
-            }
-
+    resizerNumbers: function(number,loop) {
+        if (loop == 4) {
+            number = number.replace(/^(\d)$/g, '000$1');
+            number = number.replace(/^(\d)(\d)$/g, '00$1$2');
+            number = number.replace(/^(\d)(\d)(\d)$/g, '0$1$2$3');
         }
-        // (key == "colon") ? numbers = numbers.replace(":","˙") : numbers = numbers.replace(key,sNumb[key]);
-        // (key == "dot")   ? numbers = numbers.replace(".","`") : numbers = numbers.replace(key,sNumb[key]);
-        // (key == "plus")  ? numbers = numbers.replace("+","⁺") : numbers = numbers.replace(key,sNumb[key]);
-        // (key == "minus") ? numbers = numbers.replace("-","⁻") : numbers = numbers.replace(key,sNumb[key]);
-    }
-    //console.log(res);
-    //return res;
+        if (loop == 3) {
+            number = number.replace(/^(\d)(\d)$/g, '0$1$2');
+            number = number.replace(/^(\d)$/g, '00$1');
+            
+        }
+        if (loop == 2) {
+            number = number.replace(/^(\d)$/g, '0$1');
+        }
+            return number;
+    },
+    checkSkills: function(text) {
+        var skills = {
+            PERK_1: [
+                "booster_pouch",
+                "field_medicine",
+                "advanced_training",
+                "heavyweight_marathon",
+                "battle_hardening",
+                "strong_nerves",
+                "hemoglobin_serum",
+                "second_wind",
+                "take_aim",
+                "well_rested",
+                "self_healing",
+                "will_to_live",
+                "healing_factor",
+                "sixth_sense",
+                "cold_blood",
+                "adrenaline_rush",
+                "dead_silence",
+                "in_the_crosshairs"
+            ],
+            PERK_2: [
+                "fireres_materials",
+                "subdermal_meldonium",
+                "shrapnel_layer",
+                "subdermal_morphine",
+                "regenerative_materials",
+                "adaptive_armor",
+                "tight_fit",
+                "combined_armor",
+                "head_protection",
+                "lightweight_armor",
+                "stay_frosty",
+                "stim_medpacks",
+                "inner_strength"
+            ],
+            PERK_3: [
+                "forend_processing",
+                "barrel_cutting",
+                "quick_release_magazines",
+                "direct_acting_shutter",
+                "super_sensitive_trigger",
+                "armor_piercing_rounds",
+                "heavy_ammunition",
+                "thermal_imager",
+                "expansive_bullets",
+                "flatness",
+                "tungsten_coating",
+                "consumables_pouch",
+                "prudence",
+                "strong_stim",
+                "robotic_calibrations",
+                "heavy_barrel",
+                "lightweight_equipment"
+        
+            ],
+            PERK_4: [
+                "spare_syringe",
+                "improved_formula",
+                "cold_math",
+                "altitude_training",
+                "stealth_warrior",
+                "ambush",
+                "blood_rage",
+                "counter_attack",
+                "head_hunter",
+                "retaliation",
+                "lone_wolf",
+                "fast_revive",
+                "merciless",
+                "die_hard",
+                "shoulder_to_shoulder",
+                "own_priorities"
+            ]
+        };
+        for (var key in skills) {
+            var res = "";
+            for (var i = 0; i < skills[key].length-1;i++) {
+               if (skills[key][i] == text) {
+                   // console.log(key,"-",i+1);
+                     res = key+" "+(i+1);
+                    return res;
+               }
+               if (text == "null") {
+                    res = key+" "+0;
+                    return res;
+               }
+            }
+        }
+    },
+    convertNumbers: function(numbers) {
+    var sNumb = {
+        0: "{Chr(186)}",
+        1: "{Chr(185)}",
+        2: "{Chr(178)}",
+        3: "{Chr(179)}",
+        4: "{Chr(8308)}",
+        5: "{Chr(8309)}",
+        6: "{Chr(8310)}",
+        7: "{Chr(8311)}",
+        8: "{Chr(8312)}",
+        9: "{Chr(8313)}",
+        10: "{Chr(729)}",
+        11: "{Chr(96)}",
+        12: "{Chr(8314)}",
+        13: "{Chr(8315)}"
+    };
+    numbers = numbers.split('');
+    for (var i = 0; i < numbers.length; i++) {
+        for (var value in sNumb) {
+               if (numbers[i].indexOf(":") >= 0) {numbers[i] = numbers[i].replace(/\:/g, sNumb[10]); break; 
+        } else if (numbers[i].indexOf(".") >= 0) {numbers[i] = numbers[i].replace(/\./g, sNumb[11]); break;
+        } else if (numbers[i].indexOf("+") >= 0) {numbers[i] = numbers[i].replace(/\+/g, sNumb[12]); break;
+        } else if (numbers[i].indexOf("-") >= 0) {numbers[i] = numbers[i].replace(/\-/g, sNumb[13]); break;
+        } else if (numbers[i] == sNumb[i]){
+        numbers[i] = numbers[i].replace(numbers[i], sNumb[i]);break;  
+        }{
+        }{
+        }{
+        }  
+    }}
 
-},
-convertNumbers: function(numbers) {
-    // RoboTaskApp().AddUserVariable("NUMBERS","{NUMBERS}");
- //var numbers = numbers;
- //console.log(typeof(numbers));
- var res="";
- //var numbers = "1234567890-+";
-     numbers = numbers.replace("0", '{Chr(186)}');
-     //res = numbers.replace("0","⁰");//"{Chr(186)}");
-     numbers = numbers.replace("1","{Chr(178)}");
-    //  numbers = numbers.replace("1","{Chr(178)}");
-    //  numbers = numbers.replace("2","{Chr(179)}");
-    //  numbers = numbers.replace("3","³");//"{Chr(8307)}");
-    //  numbers = numbers.replace("4","{Chr(8308)}");
-    //  numbers = numbers.replace("5","{Chr(8309)}");
-    //  numbers = numbers.replace("6","{Chr(8310)}");
-    //  numbers = numbers.replace("7","{Chr(8311)}");
-    //  numbers = numbers.replace("8","{Chr(8312)}");
-    //  numbers = numbers.replace("9","{Chr(8313)}");
-    //  numbers = numbers.replace(":","˙");
-    //  numbers = numbers.replace(".","`");
-    //  numbers = numbers.replace("+","⁺");
-    //  numbers = numbers.replace("-","⁻");
-     console.log(numbers.trim());
+    numbers = numbers.join(",").replace(/\,/g, '');
     return numbers;
-    }
-};
-caliber.convertNumbers("01");
+}};
 
-//replaceNumbers("123");
+console.log(caliber.convertNumbers("2"));
