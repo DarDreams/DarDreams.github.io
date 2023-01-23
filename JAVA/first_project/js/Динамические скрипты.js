2964733f4ad0178e0e06d2dcf31a0d62e988ea74
DarDreams <dardreams@gmail.com>
2023-01-23 15:29:29 +0100

    23.01.2023 15:29:28

---
+++ JAVA/first_project/js/Динамические скрипты.js
@@ -0,0 +1,13 @@
+//defer script //после дома
+//async script //сразу же
+
+
+
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.append(script);
+}
+
+loadScript("js/test.js");
