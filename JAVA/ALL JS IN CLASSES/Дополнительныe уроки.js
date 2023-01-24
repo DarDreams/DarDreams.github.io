de6454220886d4e1b123c691301e20b929ed76e3
DarDreams <dardreams@gmail.com>
2023-01-24 14:59:36 +0100

    24.01.2023 14:59:36

--- JAVA/ALL JS IN CLASSES/Дополнительныe уроки.js
+++ JAVA/ALL JS IN CLASSES/Дополнительныe уроки.js
@@ -172,3 +172,55 @@ map.set(shops[0],5000)
    .set(shops[1],15000)
    .set(shops[2],25000);
 console.log(map);
+////////////////////SET униккальный массив без повторений////////////////////////////
+const arr = ['Alex', 'Anna', 'Oleg', 'Alex'];
+
+const set = new Set(arr);
+
+set.add('Ivan');
+set.add('Ivan');
+
+console.log(set);
+
+// set.delete(value);
+// set.has(value);
+// set.clear();
+// set.size;
+
+// for (const value of set) {console.log(value);}
+
+// set.forEach((value, valueAgain, set) => {
+//     console.log(value, valueAgain);
+// });
+
+// console.log(set.values());
+// console.log(set.keys());
+// console.log(set.entries());
+
+function unique(arr) {
+    return Array.from(new Set(arr));
+}
+
+console.log(unique(arr));
+
+
+
+// В каждой книге есть n страниц с номерами страниц от 1 до n. Написать функцию amountOfPages, аргумент которой summary составляется путем сложения количества цифр всех номеров страниц. Эта функция возвращает число - количество страниц n в книге. Чтобы было понятно что такое количество цифр, давайте рассмотрим примеры.
+// Пример:
+// Если на входе функции summary = 25, то на результат должен быть 17. Всего в числах от 1 до 17 содержится 25 цифр: 1234567891011121314151617.
+// Функция на вход как раз принимает это общее количество цифр, а возвращает конечное число, то есть последнюю страницу книги.
+//         amountOfPages(5) => 5
+//         amountOfPages(25) => 17
+//         amountOfPages(1095) => 401   
+//         amountOfPages(185) => 97
+
+function amountOfPages(summary){
+    
+    for (let i = 0; i < summary.length; i++) {
+        const element = summary[i];
+        
+    }
+    return 
+}
+
+console.log(amountOfPages(5));
