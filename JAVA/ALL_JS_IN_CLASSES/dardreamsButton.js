b310bb4331d5a241f66392af7a727a2e61169c7b
DarDreams <dardreams@gmail.com>
2023-02-01 15:52:55 +0100

    01.02.2023 15:52:55

--- JAVA/ALL_JS_IN_CLASSES/dardreamsButton.js
+++ JAVA/ALL_JS_IN_CLASSES/dardreamsButton.js
@@ -1,10 +1,12 @@
-const dardreamsButton = document.createElement('div');
+//const dardreamsButton = document.createElement('div');
 let checkStateDarDreamsButton = false;
-dardreamsButton.innerHTML = '<img class = "dardreamsButton" style = "height: 25px; width: 25px;opacity:0.5 ;position: fixed;z-index: 999999;cursor:pointer;border-radius: 20%" src="https://sun6-20.userapi.com/s/v1/if2/mSxSLzTakqUQvgIVOS2BZFXUmIgmz63Plwrlz4f70Qjr0fr_EZhrlPSEdomh-JdSv-ZK2c5PreRmr6JBYV2C8BJf.jpg?size=50x50&amp;quality=96&amp;crop=60,155,385,385&amp;ava=1" class="AvatarRich__img">';
+//dardreamsButton.innerHTML = ;
 
-document.body.prepend(dardreamsButton);
+document.body.insertAdjacentHTML('afterbegin','<img class = "dardreamsButton" style = "height: 25px; width: 25px;opacity:0.5 ;position: fixed;z-index: 999999;cursor:pointer;border-radius: 20%" src="https://sun6-20.userapi.com/s/v1/if2/mSxSLzTakqUQvgIVOS2BZFXUmIgmz63Plwrlz4f70Qjr0fr_EZhrlPSEdomh-JdSv-ZK2c5PreRmr6JBYV2C8BJf.jpg?size=50x50&amp;quality=96&amp;crop=60,155,385,385&amp;ava=1" class="AvatarRich__img">');
+
+//document.body.prepend(dardreamsButton);
 	
-dardreamsButton.addEventListener('click', () => {
+dardreamsButton.onclick = function () {
 	if (checkStateDarDreamsButton == false) {
 		document.querySelector('.dardreamsButton').style.opacity='1';
 		changeDarDreamsButton(true);
@@ -14,4 +16,4 @@ dardreamsButton.addEventListener('click', () => {
 	changeDarDreamsButton(false);	
 	checkStateDarDreamsButton = false;
 	}
-});
+};
--- JAVA/ALL_JS_IN_CLASSES/youtube.js
+++ JAVA/ALL_JS_IN_CLASSES/youtube.js
@@ -1,9 +1,9 @@
-
+console.clear();
+// v3.1
+let createList;
 let global;
-let firstBorder;
 let listSeconds;
 let inText;
-let currentTime;
 let pause;
 let divPause;
 let divVolume;
@@ -14,213 +14,140 @@ let divButtonPrev;
 let divTimer;
 let total;
 let iElement;
-
+let timerPlay;
+let now;
+let divLink;
+let divDardreams;
+let mlines = [];
 
 function changeDarDreamsButton(bool) {
 	if (bool) {
-		document.querySelector('.dardreamsButton').style.opacity='1';
-		//checkStateMyButton = true;
 		global();
-		
 	} else {
-		document.querySelector('.dardreamsButton').style.opacity='0.5';
-		//checkStateMyButton = false;
-		document.body.firstChild.style.display='none';
 		document.querySelectorAll('#comment-content')[0].style = '';
 		document.body.style.overflow ='';
-		
-		
+		divDardreams.remove();
+		clearTimeout(timerPlay);
 	}
 }
 
-
 ////////////////////////////////
-
-
 global = function () {
-document.documentElement.scrollTop = 300;
-let timer = setInterval(overlay, 1000);
-let timerPlay = setInterval(currentTime, 1000);
-let video = document.getElementsByTagName("video")[0];
+	document.documentElement.scrollTop = 300;
+	let timer = setInterval(overlay, 1000);
+	timerPlay = setInterval(currentTime, 500);
+	let video = document.getElementsByTagName("video")[0];
 
-
-
-//////////////////////////////////////////////////////
-
-
-
-firstBorder = function () {
-	for (iElement = 0; iElement < createList().length; iElement++) {
-        if (video.currentTime < listSeconds[iElement]) {
-            border(iElement);
-            return;
-        }
-    }   
-}
-
-inText = function (stringTime) {
-    let date = new Date();
-    let res = "";
-    res = date.setTime(stringTime*1000-3600*1000);
-    res = `${getZero(date.getHours())}:${getZero(date.getMinutes())}:${getZero(date.getSeconds())}`;
-    return res;
-}
-
-currentTime = function () {
-   document.querySelector("#myTimer").innerText = inText(video.currentTime);
-}
-
-
-function hide(string){
-	document.querySelector(string).style.display = 'none';
-}
-
-  function overlay() {
-    divOverlay    = document.createElement('div');
-    divButtonNext = document.createElement('div');
-    divButtonPrev = document.createElement('div');
-    divTimer      = document.createElement('div');
-    divVolume     = document.createElement('div');
-    divPause      = document.createElement('div');
-    divComments   = document.querySelectorAll('#comment-content')[0];
-          
-    
-    if (divComments) {
-        clearTimeout(timer);
-        divOverlay.innerHTML = "<div class='overlayBlack' style='position: fixed; height: 100vh;width: 100vw;background: black;pointer-events: none; z-index: 999;opacity: 0.8;'></div>";
-        document.body.prepend(divOverlay);
-        divButtonNext.innerHTML = `<div class = "myButtons" style="position:absolute;width: 126px;height: 55px;color: red;z-index: 99999;font-size: 50px;cursor: pointer;left: 1086px;top: 302px;border: 4px solid red;">NEXT</div>`;
-        divButtonPrev.innerHTML = `<div class = "myButtons" style="position:absolute;width: 126px;height: 55px;color: red;z-index: 99999;font-size: 50px;cursor: pointer;left: 813px;top: 302px;border: 4px solid red;">PREV</div>`;
-        divTimer.innerHTML = `<div id = "myTimer" style="position:absolute;width: 196px;height: 55px;color: red;z-index: 99999;font-size: 50px;left: 849px;top: 198px;font-family: Consolas;font-size: 76px;">01:35:17</div>`;
-        divVolume.innerHTML = `<input id = "myVolume" type="range" id="volume" name="volume" min="0" max="100" style="position: absolute;top: 431px;z-index: 99999; left: 987px;">`;
-        divOverlay.prepend(divButtonNext);
-        divOverlay.prepend(divButtonPrev);
-        divOverlay.prepend(divTimer);
-        
-        divOverlay.prepend(divVolume);
-        divComments.style.zIndex = 9999;
-        divComments.style.marginTop = `-155vh`;
-        divComments.style.width = '1000px';
-        document.documentElement.scrollTop = 0;
-        divVolume.value = video.volume*100;
-    }
-    divButtonNext.addEventListener('click',() => {
-        next();
-    });
-
-
-    divButtonPrev.addEventListener('click',() => {
-        prev();
-    });
-
-    divPause.addEventListener('click',() => {
-        pause();
-    });
-    
-    divVolume.addEventListener('input',() => {
-        video.volume = document.querySelector('#myVolume').value / 100;
-    });
-
-    createList();
-    document.querySelector("#more > span").click();
-    firstBorder();
-    document.documentElement.scrollTop = 0;
-    document.body.style.overflow = "hidden";
-    hide('#detach-button-host');
-    hide('#masthead-container');
-    hide('#toolbar');
-    console.clear();
-}
-
-
-
-
-
-function getZero (num) {
-    if (num >= 0 && num < 10) {
-        return `0${num}`;
-    } else {
-        return num;
-    }
-}
-
-
-function createList() {
-    total = divComments.querySelectorAll('a').length;
-    let divLink = document.querySelectorAll('#comment-content a');
-    let mlines = [];
-    for (var i = 1; i < total; i++) {
-    	
-    	divLink.onclick = function (event){
-  	    firstBorder();
-    	}
-    mlines.push(`${divLink[i].innerHTML}`);
-    }
-    listSeconds = mlines.map(item => inSeconds(item.replaceAll("'","")));
-    return mlines;
-}
-
-
-function inSeconds(time) {
-    let hms = time;   
-    let a = hms.split(':'); 
-    let seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]); 
-    return seconds;
-}
-
-
-function click(time) {
-    for (const a of document.querySelectorAll("a")) {
-        if (a.textContent.includes(time)) {
-          a.click();
-          document.body.style.overflow = "hidden";
-        }
-      }
-}
-
-
-function border(arg) {
-
-	let time = divComments.querySelectorAll('a');
-	time[arg].nextElementSibling.style.border = "4px solid red";
-    time[arg].nextElementSibling.style.borderLeft = "transparent";
-    time[arg].nextElementSibling.style.borderRight = "transparent";
-    time[arg].nextElementSibling.style.paddingTop = "1px";
-    time[arg].nextElementSibling.style.paddingBottom = "2px";
-    
-    time[arg].style.border = "4px solid red";
-    time[arg].style.borderRight = "transparent";
-    time[arg].style.borderLeft = "transparent";
-    time[arg-1].nextElementSibling.style.border = "none";
-    time[arg-1].style.border = "none";
-    time[arg+1].nextElementSibling.style.border = "none";
-    time[arg+1].style.border = "none";
-    
-}
-
-function next() {
-    for (let i = 0; i < createList().length; i++) {
-        if (video.currentTime < listSeconds[i]) {
-            click(createList()[i]);
-            border(i+1);
-            //time[iElement].nextElementSibling.style.border = "none";
-            //time[iElement].style.border = "none";
-            return;
-        }
-    }   
-}
-
-function prev() {
-    for (let i = 0; i < createList().length; i++) {
-        if (video.currentTime < listSeconds[i]) {
-            click(createList()[i-2]);
-            border(i-1);
-            time[iElement].nextElementSibling.style.border = "none";
-            time[iElement].style.border = "none";
-            return;
-        }
-    }   
-}
+	function border() {
+		let time = document.querySelectorAll('#comment-content')[0].querySelectorAll('a');
+		for (i = 1; i < createList().length; i++) {
+	        if (video.currentTime < listSeconds[i] && video.currentTime >= listSeconds[i-1]) {
+	        	now = i;
+				//link
+				time[i].nextElementSibling.style.cssText =  `
+				border: 4px solid red;
+				border-left: transparent;
+				border-right: transparent;
+				padding-top: 1px;
+				padding-bottom: 2px;`;
+				//text
+				time[i].style.cssText = `
+				border: 4px solid red;
+				border-right: transparent;
+				border-left: transparent;`;
+	        } else {
+			    	time[i].style.border = "";
+			    	time[i].nextElementSibling.style.border = '';
+			    
+	        }
+	    }   
+	}
+	
+	inText = function (stringTime) {
+	    let date = new Date();
+	    let res = "";
+	    res = date.setTime(stringTime*1000-3600*1000);
+	    res = `${getZero(date.getHours())}:${getZero(date.getMinutes())}:${getZero(date.getSeconds())}`;
+	    return res;
+	}
+	
+	function currentTime() {
+	   document.querySelector(".myTimer").innerText = inText(video.currentTime);
+	   border();
+	}
+	
+	function hide(string){
+		document.querySelector(string).style.display = 'none';
+	}
+	
+	  function overlay() {
+		divComments   = document.querySelectorAll('#comment-content')[0];
+	    if (divComments) {
+	        clearTimeout(timer);
+	        document.body.insertAdjacentHTML('afterBegin', "<div class = 'dardreams'></div>");
+	        divDardreams = document.querySelector('.dardreams');
+	        divDardreams.insertAdjacentHTML('afterBegin',`<div class = "myButtons myNext" style="position:absolute;width: 126px;height: 55px;color: red;z-index: 99999;font-size: 50px;cursor: pointer;left: 1086px;top: 302px;border: 4px solid red;">NEXT</div>`);
+	        divDardreams.insertAdjacentHTML('afterBegin',`<div class = "myButtons myPrev" style="position:absolute;width: 126px;height: 55px;color: red;z-index: 99999;font-size: 50px;cursor: pointer;left: 813px;top: 302px;border: 4px solid red;">PREV</div>`);
+	        divDardreams.insertAdjacentHTML('afterBegin',`<div class = "myTimer" style="position:absolute;width: 196px;height: 55px;color: red;z-index: 99999;font-size: 50px;left: 849px;top: 198px;font-family: Consolas;font-size: 76px;">01:35:17</div>`);
+	        divDardreams.insertAdjacentHTML('afterBegin',`<input class = "myVolume" type="range" id="volume" name="volume" min="0" max="100" style="position: absolute;top: 431px;z-index: 99999; left: 987px;">`);
+	        divDardreams.insertAdjacentHTML('afterBegin', "<div class='overlayBlack' style='position: fixed; height: 100vh;width: 100vw;background: black;pointer-events: none; z-index: 999;opacity: 0.8;'></div>");
+	         divOverlay    = document.querySelector('.overlayBlack');
+			 divButtonNext = document.querySelector('.myNext');
+			 divButtonPrev = document.querySelector('.myPrev');
+			 divTimer      = document.querySelector('.myTimer');
+			 divVolume     = document.querySelector('.myVolume');
+		        
+	        divComments.style.zIndex = 9999;
+	        divComments.style.marginTop = `-155vh`;
+	        divComments.style.width = '1000px';
+	        document.documentElement.scrollTop = 0;
+	        divVolume.value = video.volume*100;
+	    };
+	    /////TRIGGERS
+		divButtonNext.onclick = function() {
+			divComments.querySelectorAll('a')[now+1].click();
+		};
+	
+	    divButtonPrev.onclick = function() {
+	        divComments.querySelectorAll('a')[now-1].click();
+	    };
+	
+	    divVolume.oninput = function() {
+	        video.volume = document.querySelector('.myVolume').value / 100;
+	    };
+		/////////////
+	    createList();
+	    document.querySelector("#more > span").click();
+	    document.documentElement.scrollTop = 0;
+	    document.body.style.overflow = "hidden";
+	    hide('#detach-button-host');
+	    hide('#masthead-container');
+	    hide('#toolbar');
+	}
+	
+	function getZero (num) {
+	    if (num >= 0 && num < 10) {
+	        return `0${num}`;
+	    } else {
+	        return num;
+	    }
+	}
+	
+createList = function () {
+		mlines = [];
+	    total = divComments.querySelectorAll('a').length;
+	    divLink = document.querySelectorAll('#comment-content a');
+	    for (var i = 1; i < total; i++) {
+	    	mlines.push(`${divLink[i].innerHTML}`);
+	    }
+	    listSeconds = mlines.map(item => inSeconds(item.replaceAll("'","")));
+	    return mlines;
+	}
+	
+	function inSeconds(time) {
+	    let a = time.split(':'); 
+	    return (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]); 
+	}
 
 }
 
