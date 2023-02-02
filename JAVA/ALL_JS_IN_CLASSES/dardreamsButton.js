6db16c256656bdc0f10908cb45ea9f3c01d9378a
DarDreams <dardreams@gmail.com>
2023-02-02 08:37:42 +0100

    dardreamsButton

--- JAVA/ALL_JS_IN_CLASSES/dardreamsButton.js
+++ JAVA/ALL_JS_IN_CLASSES/dardreamsButton.js
@@ -1,11 +1,7 @@
-//const dardreamsButton = document.createElement('div');
-let checkStateDarDreamsButton = false;
-//dardreamsButton.innerHTML = ;
-
 document.body.insertAdjacentHTML('afterbegin','<img class = "dardreamsButton" style = "height: 25px; width: 25px;opacity:0.5 ;position: fixed;z-index: 999999;cursor:pointer;border-radius: 20%" src="https://sun6-20.userapi.com/s/v1/if2/mSxSLzTakqUQvgIVOS2BZFXUmIgmz63Plwrlz4f70Qjr0fr_EZhrlPSEdomh-JdSv-ZK2c5PreRmr6JBYV2C8BJf.jpg?size=50x50&amp;quality=96&amp;crop=60,155,385,385&amp;ava=1" class="AvatarRich__img">');
+let dardreamsButton = document.querySelector('.dardreamsButton');
+let checkStateDarDreamsButton = false;
 
-//document.body.prepend(dardreamsButton);
-	
 dardreamsButton.onclick = function () {
 	if (checkStateDarDreamsButton == false) {
 		document.querySelector('.dardreamsButton').style.opacity='1';
