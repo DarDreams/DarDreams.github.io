3d3f58c1354c5030821e65ddaf61af2a6156cba8
DarDreams <dardreams@gmail.com>
2023-02-09 18:51:01 +0100

    09.02.2023 18:51:00

--- Caliber/js/script.js
+++ Caliber/js/script.js
@@ -42,12 +42,27 @@ window.addEventListener('DOMContentLoaded', () => {
      console.log(game.team1.gunner.perks.split(",")[2]);
      console.log(game.team1.gunner.perks.split(",")[3]);
      
-    for (let i = 0; i < 3; i++) {
+    let assPerks1 = game.team1.assault.perks.split(",");
+    let gunPerks1 = game.team1.gunner.perks.split(",");
+    let medPerks1 = game.team1.medic.perks.split(",");
+    let sniPerks1 = game.team1.sniper.perks.split(",");
+
+    let assPerks2 = game.team2.assault.perks.split(",");
+    let gunPerks2 = game.team2.gunner.perks.split(",");
+    let medPerks2 = game.team2.medic.perks.split(",");
+    let sniPerks2 = game.team2.sniper.perks.split(",");
+
+    for (let i = 0; i < 4; i++) {
+        console.log(assPerks1[i]);
+        team1Selector[1].children[4].insertAdjacentHTML('afterbegin',`<svg class="skill-ico"><title>${assPerks1[i]}</title><use xlink:href="#${assPerks1[i]}"></use></svg>`);
+        team1Selector[2].children[4].insertAdjacentHTML('afterbegin',`<svg class="skill-ico"><title>${gunPerks1[i]}</title><use xlink:href="#${gunPerks1[i]}"></use></svg>`);
+        team1Selector[3].children[4].insertAdjacentHTML('afterbegin',`<svg class="skill-ico"><title>${medPerks1[i]}</title><use xlink:href="#${medPerks1[i]}"></use></svg>`);
+        team1Selector[4].children[4].insertAdjacentHTML('afterbegin',`<svg class="skill-ico"><title>${sniPerks1[i]}</title><use xlink:href="#${sniPerks1[i]}"></use></svg>`);
         
-        team1Selector[1].children[4].insertAdjacentHTML('afterbegin',`<svg class="skill-ico"><title>test</title><use xlink:href="#${game.team1.assault.perks.split(",")[i]}"></use></svg>`);
-        team1Selector[2].children[4].insertAdjacentHTML('afterbegin',`<svg class="skill-ico"><use xlink:href="#${game.team1.gunner.perks.split(",")[i]}"></use></svg>`);
-        team1Selector[3].children[4].insertAdjacentHTML('afterbegin',`<svg class="skill-ico"><use xlink:href="#${game.team1.medic.perks.split(",")[i]}"></use></svg>`);
-        team1Selector[4].children[4].insertAdjacentHTML('afterbegin',`<svg class="skill-ico"><use xlink:href="#${game.team1.sniper.perks.split(",")[i]}"></use></svg>`);
+        team1Selector[5].children[4].insertAdjacentHTML('afterbegin',`<svg class="skill-ico"><title>${assPerks2[i]}</title><use xlink:href="#${assPerks2[i]}"></use></svg>`);
+        team1Selector[6].children[4].insertAdjacentHTML('afterbegin',`<svg class="skill-ico"><title>${gunPerks2[i]}</title><use xlink:href="#${gunPerks2[i]}"></use></svg>`);
+        team1Selector[7].children[4].insertAdjacentHTML('afterbegin',`<svg class="skill-ico"><title>${medPerks2[i]}</title><use xlink:href="#${medPerks2[i]}"></use></svg>`);
+        team1Selector[8].children[4].insertAdjacentHTML('afterbegin',`<svg class="skill-ico"><title>${sniPerks2[i]}</title><use xlink:href="#${sniPerks2[i]}"></use></svg>`);
     }
     
 
