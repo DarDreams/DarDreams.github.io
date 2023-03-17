e41c58b048bc4c7684aa331f26212f59000f5040
DarDreams <dardreams@gmail.com>
2023-03-18 00:54:23 +0100

    18.03.2023 0:54:22

--- Caliber/js/script.js
+++ Caliber/js/script.js
@@ -925,7 +925,7 @@ try {
                 // console.log("[3][0] - |",operator.perks[3],"|");
                 // console.log("data[0] - ", data1);
 
-                
+                console.log(caliber[k+5][i][2]," - ",caliber[k+5][i][16][1]);
 
 
                 let img = new Image();
