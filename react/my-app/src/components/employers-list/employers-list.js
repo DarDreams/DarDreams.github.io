5a62e78f69bf5930f41ce268035ce4a6e7074c7c
DarDreams <dardreams@gmail.com>
2023-03-23 12:34:03 +0100

    23.03.2023 12:34:02

--- react/my-app/src/components/employers-list/employers-list.js
+++ react/my-app/src/components/employers-list/employers-list.js
@@ -1,13 +1,18 @@
 import EmployersListItem from "../employers-list-item/employers-list-item"
 import './employers-list.css'
 
-const EployersList = ({data}) => {
+const EployersList = ({data, onDelete, onToggleProp}) => {
 
     const elements = data.map(item => {
     const {id, ...itemProps} = item;
         return (
             //<EmployersListItem name={item.name} salary={item.salary}/>
-            <EmployersListItem key={id}  {...itemProps}/>
+            <EmployersListItem 
+                key={id}  
+                {...itemProps}
+                onDelete={() => onDelete(id)}
+                onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}
+            />
             
         )
     })
