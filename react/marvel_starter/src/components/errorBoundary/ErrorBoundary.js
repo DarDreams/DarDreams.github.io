4c88bad7841caec253ffbdca801d83372df9fc69
DarDreams <dardreams@gmail.com>
2023-03-30 11:41:40 +0200

    30.03.2023 11:41:39

--- react/marvel_starter/src/components/errorBoundary/ErrorBoundary.js
+++ react/marvel_starter/src/components/errorBoundary/ErrorBoundary.js
@@ -1,9 +1,15 @@
 import { Component } from "react";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+
 
 class ErrorBoundary extends Component {
     state = {
         error: false
     }
+
+    // static getDerivedStateFromError(error) {
+    //     return {error: true}
+    // }
 
     componentDidCatch(error, errorInfo) {
         console.log(error, errorInfo);
@@ -14,7 +20,7 @@ class ErrorBoundary extends Component {
 
     render() {
         if (this.state.error) {
-            return <h2>Something went wrong</h2>
+            return <ErrorMessage/>
         }
         return this.props.children;
     }
