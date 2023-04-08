da448ba62452f2ebfdcc3b0316d20fb5cf65153a
DarDreams <dardreams@gmail.com>
2023-04-08 23:52:07 +0200

    08.04.2023 23:52:07

--- Caliber/php/query.php
+++ Caliber/php/query.php
@@ -2,7 +2,7 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode(file_get_contents('php://input'), true);
     $json = json_encode($data, JSON_PRETTY_PRINT);
-    file_put_contents('../game.json', $json);
+    file_put_contents('../games.json', $json);
     echo $json;
 }
 ?>
